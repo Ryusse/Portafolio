898566c341aa7c05b3252dc663cf5affd8ab374c
Ryusse <joelangeltw@gmail.com>
2021-01-28 17:17:47 -0500

    Portfolio: error script

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+//Portfolio item filter
+
+const filterContainer= document.querySelector(".portfolio-filter"),
+      filterBtns=filterContainer.children,
+      totalFilterBtn=filterBtns.length,
+      portfolioItems=document.querySelectorAll(".portfolio-item"),
+      totalPorfolioItem=portfolioItems.length;
+
+
+      for(let i=0 ; i<totalFilterBtn ; i++){
+          filterBtns[i].addEventListener("click", function(){
+            filterContainer.querySelector(".active").classList.remove("active");
+              this.classList.add("active");
+
+              const filterValue=this.getAttribute("data-filter");
+
+              for(let k=0; k<totalPorfolioItem; k++){
+
+                  if(filterValue === portfolioItems[k].getAttribute("data-category")){
+                      portfolioItems[k].classList.remove("hide");
+                      portfolioItems[k].classList.add("show");
+                  }else{
+                      portfolioItems[k].classList.remove("show");
+                      portfolioItems[k].classList.add("hide")
+                  }
+
+                  if(filterValue==="all"){
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                  }
+              }
+          })
+      }
+
+      //Portfolio Lightbox
+
+    const lightbox=document.querySelector(".lightbox"),
+          lightboxImg=lightbox.querySelector(".lightbox-img"),
+          lightboxText=lightbox.querySelector(".caption-text"),
+          lightboxCounter=lightbox.querySelector(".caption-counter");
+    
+    let itemIndex=0;
+
+    for(let i=0;i<totalPorfolioItem;i++){
+        portfolioItems[i].addEventListener("click",function(){
+            itemIndex=i;
+            changeItem()
+            toggleLightbox();
+        })
+    }
+
+    function toggleLightbox(){
+        lightbox.classList.toggle("open");
+    }
+
+    function changeItem(){
+        imgSrc=portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+    
+        lightboxImg.src=imgSrc;
+        lightboxText.innerHTML=portfolioItems[itemIndex].querySelector("h4").innerHTML;
+        lightboxCounter.innerHTML= (itemIndex+1) + "of" + totalPorfolioItem;
+
+    }
