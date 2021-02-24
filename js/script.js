8d15b966d2a1e5492c1ab0574cc9d817ed3aa63f
Ryusse <joelangeltw@gmail.com>
2021-02-24 15:32:27 -0500

    Finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+
+window.addEventListener("load",function(){
+    document.querySelector(".preloader").classList.add("opacity-0");
+    setTimeout(function(){
+        document.querySelector(".preloader").style.display="none";
+    },1000)
+})
 //Portfolio item filter
 
 const filterContainer= document.querySelector(".portfolio-filter"),
@@ -85,8 +92,102 @@ const filterContainer= document.querySelector(".portfolio-filter"),
 
     //close Lightbox
 
-   light.box.addEventListener("click",function(event){
+   lightbox.addEventListener("click",function(event){
        if(event.target === lightboxClose|| event.target === lightbox){
            toggleLightbox();
        }
    })
+
+
+   //Aside Navbar
+
+   const nav=document.querySelector(".nav"),
+         navList=nav.querySelectorAll("li"),
+         totalNavList=navList.length,
+         allSection=document.querySelectorAll(".section"),
+         totalSection=allSection.length;
+
+    for(let i=0 ; i<totalNavList ; i++){
+        const a=navList[i].querySelector("a");
+        
+        a.addEventListener("click",function(){
+
+            //remove back section class
+            removeBackSectionClass();
+
+            for(let j=0 ; j<totalNavList ; j++){
+                if(navList[j].querySelector("a").classList.contains("active")){
+                //add back section cñass
+                    addBackSectionClass(j);
+                }
+                navList[j].querySelector("a").classList.remove("active");
+
+            }
+            this.classList.add("active");
+            showSection(this);
+
+            if(window.innerWidth < 1200){
+                asideSectionTogglerBtn();
+            }
+
+        })
+    }
+
+    function removeBackSectionClass(){
+        for(let i=0;i<totalSection;i++){
+            allSection[i].classList.remove("back-section");
+        }
+    }
+
+    function addBackSectionClass(num){
+        allSection[num].classList.add("back-section");
+    }
+
+
+    function showSection(element){
+        for(let i=0;i<totalSection ; i++){
+            allSection[i].classList.remove("active");
+        }
+       const target=element.getAttribute("href").split("#")[1];
+       document.querySelector("#"+target).classList.add("active") 
+    }
+
+   
+
+    function updateNav(element){
+        for(let i=0 ; i<totalNavList ; i++){
+            navList[i].querySelector("a").classList.remove("active");
+            const target=element.getAttribute("href").split("#")[1];
+            if(target == navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+                navList[i].querySelector("a").classList.add("active");
+
+            }
+        }
+        
+    }
+
+    document.querySelector(".hire-me").addEventListener("click",function(){
+        const sectionIndex = this.getAttribute("data-section-index");
+        sectionIndex
+        showSection(this);
+        updateNav(this);
+        removeBackSectionClass();
+        addBackSectionClass(sectionIndex);
+
+    })
+
+
+    const navTogglerBtn=document.querySelector(".nav-toggle"),
+          aside=document.querySelector(".aside");
+
+    navTogglerBtn.addEventListener("click",()=>{
+        asideSectionTogglerBtn();
+    })
+
+    function asideSectionTogglerBtn(){
+        aside.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+        for(let i=0;i<totalSection ; i++){
+            allSection[i].classList.toggle("open");
+        }
+    }
